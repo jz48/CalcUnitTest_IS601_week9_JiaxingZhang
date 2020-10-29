61b2e7ccf950e0f5696a4d415ba22672e8fab6bd
tabzhangjx <tabzhangjx@gmail.com>
2020-10-29 18:17:19 +0800

    FEATURE: Add calculator tester in ClacTest.py

---
+++ src/CalcTest.py
@@ -0,0 +1,15 @@
+import unittest
+from Calculator import Calculator
+from CsvReader import CsvReader
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+
+if __name__ == '__main__':
+    unittest.main()
